7a663edcef4137fceef63d9ec841c3e85ba988c5
yared2 <yardbyn480@gmail.com>
2024-09-16 14:40:21 -0400

    initial project

---
+++ src/main/java/com/gdam/FundForGedamV1Application.java
@@ -0,0 +1,13 @@
+package com.gdam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundForGedamV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundForGedamV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopme/FundForGedamV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundForGedamV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
